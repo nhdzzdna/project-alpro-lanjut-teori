8a72b05fbe2768227fd048245e2a4078d9bf10f5
nhdzzdna <nhdzzdna@gmail.com>
2025-05-19 21:04:07 +0700

    update

--- alproLanjutK1.cpp
+++ alproLanjutK1.cpp
@@ -6,19 +6,29 @@ struct id{
     int angka;
 };
 
-struct barang{
+struct barang
+{
     string nama;
+    float harga;
     int stok;
     struct id idBarang;
-}; barang Barang[1000];
+    int kategori;
+    int rak;
+    string Kategori;
+};
+
+barang Barang[1000];
 
 int menu, pilihkategori, jumlahBarang = 0, menuPerbarui, urutanBarang, AscDesc;
 char kembali;
-void MENU(), tambahBarang(), tampilkanBarang(barang Barang[], int jumlahBarang), cariBarang(), hapusBarang(), kosong();
-void menuPerbaruiBarang(), perbaruiBarang(), menuTampilkanBarang(), swapBarang(barang &a, barang &b);
+void MENU(), pilihKategori(), tambahBarang(), tampilkanBarang(barang Barang[], int jumlahBarang), cariBarang(), hapusBarang(), kosong();
+void menuPerbaruiBarang(), perbaruiBarang(), perbaruiNama(), perbaruiRak(), menuTampilkanBarang(), swapBarang(barang &a, barang &b);
 void menuAscDesc(), ascNama(barang Barang[], int jumlahBarang), descNama(barang Barang[], int jumlahBarang);
 void ascID(barang Barang[], int jumlahBarang), descID(barang Barang[], int jumlahBarang);
 void ascStok(barang Barang[], int jumlahBarang), descStok(barang Barang[], int jumlahBarang);
+void ascKategori(barang Barang[], int jumlahBarang), descKategori(barang Barang[], int jumlahBarang), ascRak(barang Barang[], int jumlahBarang), descRak(barang Barang[], int jumlahBarang);
+
+
 
 int main()
 {
@@ -93,8 +103,9 @@ void MENU()
     cout << "| 3. Perbarui Data Barang  |\n";
     cout << "| 4. Cari Barang           |\n";
     cout << "| 5. Hapus Data Barang     |\n";
-    cout << "============================\n";    
-    cout << "Pilih menu : "; cin >> menu;      
+    cout << "============================\n";
+    cout << "Pilih menu : ";
+    cin >> menu;
 }
 
 void tambahBarang()
@@ -105,9 +116,33 @@ void tambahBarang()
     cout << "===================================\n";
     cout << "Nama Barang : ";
     cin >> Barang[jumlahBarang].nama;
-    cout << "Stok : ";
-    cin >> Barang[jumlahBarang].stok;  
-    
+    cout << "Stok        : ";
+    cin >> Barang[jumlahBarang].stok;
+    cout << "===================================\n";
+    cout << "Kategori Barang" << endl;
+    cout << "-----------------------------------\n";
+    cout << "1. Makanan" << endl;
+    cout << "2. Minuman" << endl;
+    cout << "3. Pakaian" << endl;
+    cout << "4. Elektronik" << endl;
+    cout << "5. Perlengkapan rumah tangga" << endl;
+    cout << "6. Lainnya" << endl;
+    cout << "-----------------------------------\n";
+    cout << "Pilih kategori : ";
+    cin >> pilihkategori;
+    cout << "===================================\n";
+    pilihKategori();
+    int rakSementara;
+    cout << "Lokasi barang di rak (1-25) :";
+    cin >> rakSementara;
+    if (rakSementara > 25 || rakSementara < 1)
+    {
+        cout << "Lokasi rak tidak ada" << endl;
+    }else{
+        Barang[jumlahBarang].rak = rakSementara;
+    }
+    cout << "===================================\n";
+
     if(jumlahBarang<9){
         Barang[jumlahBarang].idBarang.huruf = "A0";
         Barang[jumlahBarang].idBarang.angka = jumlahBarang+1;
@@ -125,22 +160,36 @@ void pilihKategori()
     if (pilihkategori == 1)
     {
         Barang[jumlahBarang].kategori = 1;
+        Barang[jumlahBarang].Kategori = "Makanan";
     }
     else if (pilihkategori == 2)
     {
         Barang[jumlahBarang].kategori = 2;
+        Barang[jumlahBarang].Kategori = "Minuman";
     }
     else if (pilihkategori == 3)
     {
         Barang[jumlahBarang].kategori = 3;
+        Barang[jumlahBarang].Kategori = "Pakaian";
     }
     else if (pilihkategori == 4)
     {
         Barang[jumlahBarang].kategori = 4;
+        Barang[jumlahBarang].Kategori = "Elektronik";
     }
     else if (pilihkategori == 5)
     {
         Barang[jumlahBarang].kategori = 5;
+        Barang[jumlahBarang].Kategori = "Perlengkapan Rumah Tangga";
+    }
+    else if (pilihkategori == 6)
+    {
+        Barang[jumlahBarang].kategori = 6;
+        Barang[jumlahBarang].Kategori = "Lainnya";
+    }
+    else
+    {
+        cout << "Pilihan tidak ada" << endl;
     }
 }
 
@@ -153,6 +202,8 @@ void menuTampilkanBarang()
     cout << "| 1. Nama                  |\n";
     cout << "| 2. ID                    |\n";
     cout << "| 3. Stok                  |\n";
+    cout << "| 4. Kategori              |\n";
+    cout << "| 5. Lokasi Rak            |\n";
     cout << "============================\n";
     cout << "Pilih menu : ";
     cin >> urutanBarang;
@@ -178,12 +229,38 @@ void menuAscDesc()
     else if (urutanBarang == 1 && AscDesc == 2)
     {
         descNama(Barang, jumlahBarang);
-    }else if(urutanBarang==3 && AscDesc==1){
+    }
+    else if (urutanBarang == 2 && AscDesc == 1)
+    {
+        ascID(Barang, jumlahBarang);
+    }
+    else if (urutanBarang == 2 && AscDesc == 2)
+    {
+        descID(Barang, jumlahBarang);
+    }
+    else if (urutanBarang == 3 && AscDesc == 1)
+    {
         ascStok(Barang, jumlahBarang);
     }
     else if (urutanBarang == 3 && AscDesc == 2)
     {
         descStok(Barang, jumlahBarang);
+    }
+    else if (urutanBarang == 4 && AscDesc == 1)
+    {
+        ascKategori(Barang, jumlahBarang);
+    }
+    else if (urutanBarang == 4 && AscDesc == 2)
+    {
+        descKategori(Barang, jumlahBarang);
+    }
+    else if (urutanBarang == 5 && AscDesc == 1)
+    {
+        ascRak(Barang, jumlahBarang);
+    }
+    else if (urutanBarang == 5 && AscDesc == 2)
+    {
+        descRak(Barang, jumlahBarang);
     }
     else
     {
@@ -223,22 +300,30 @@ void descNama(barang Barang[], int jumlahBarang)
     tampilkanBarang(Barang, jumlahBarang);
 }
 
-void ascID(barang Barang[], int jumlahBarang){
-    for (int i=0; i<jumlahBarang-1;i++){
-        for (int j=0; j<jumlahBarang-1-i; j++){
-            if (Barang[j].idBarang.angka>Barang[j+1].idBarang.angka){
-                swapBarang(Barang[j], Barang[j+1]);
+void ascID(barang Barang[], int jumlahBarang)
+{
+    for (int i = 0; i < jumlahBarang - 1; i++)
+    {
+        for (int j = 0; j < jumlahBarang - 1 - i; j++)
+        {
+            if (Barang[j].idBarang.angka > Barang[j + 1].idBarang.angka)
+            {
+                swapBarang(Barang[j], Barang[j + 1]);
             }
         }
     }
     tampilkanBarang(Barang, jumlahBarang);
 }
 
-void descID(barang Barang[], int jumlahBarang){
-    for(int i=0; i<jumlahBarang-1; i++){
-        for(int j=0; j<jumlahBarang-1-i; j++){
-            if(Barang[j].idBarang.angka<Barang[j+1].idBarang.angka){
-                swapBarang(Barang[j], Barang[j+1]);
+void descID(barang Barang[], int jumlahBarang)
+{
+    for (int i = 0; i < jumlahBarang - 1; i++)
+    {
+        for (int j = 0; j < jumlahBarang - 1 - i; j++)
+        {
+            if (Barang[j].idBarang.angka< Barang[j + 1].idBarang.angka)
+            {
+                swapBarang(Barang[j], Barang[j + 1]);
             }
         }
     }
@@ -274,6 +359,62 @@ void descStok(barang Barang[], int jumlahBarang)
     }
     tampilkanBarang(Barang, jumlahBarang);
 }
+void ascKategori(barang Barang[], int jumlahBarang)
+{
+    for (int i = 0; i < jumlahBarang - 1; i++)
+    {
+        for (int j = 0; j < jumlahBarang - 1 - i; j++)
+        {
+            if (Barang[j].kategori > Barang[j + 1].kategori)
+            {
+                swapBarang(Barang[j], Barang[j + 1]);
+            }
+        }
+    }
+    tampilkanBarang(Barang, jumlahBarang);
+}
+void descKategori(barang Barang[], int jumlahBarang)
+{
+    for (int i = 0; i < jumlahBarang - 1; i++)
+    {
+        for (int j = 0; j < jumlahBarang - 1 - i; j++)
+        {
+            if (Barang[j].kategori < Barang[j + 1].kategori)
+            {
+                swapBarang(Barang[j], Barang[j + 1]);
+            }
+        }
+    }
+    tampilkanBarang(Barang, jumlahBarang);
+}
+void ascRak(barang Barang[], int jumlahBarang)
+{
+    for (int i = 0; i < jumlahBarang - 1; i++)
+    {
+        for (int j = 0; j < jumlahBarang - 1 - i; j++)
+        {
+            if (Barang[j].rak > Barang[j + 1].rak)
+            {
+                swapBarang(Barang[j], Barang[j + 1]);
+            }
+        }
+    }
+    tampilkanBarang(Barang, jumlahBarang);
+}
+void descRak(barang Barang[], int jumlahBarang)
+{
+    for (int i = 0; i < jumlahBarang - 1; i++)
+    {
+        for (int j = 0; j < jumlahBarang - 1 - i; j++)
+        {
+            if (Barang[j].rak < Barang[j + 1].rak)
+            {
+                swapBarang(Barang[j], Barang[j + 1]);
+            }
+        }
+    }
+    tampilkanBarang(Barang, jumlahBarang);
+}
 
 void tampilkanBarang(barang Barang[], int jumlahBarang)
 {
@@ -284,8 +425,10 @@ void tampilkanBarang(barang Barang[], int jumlahBarang)
     for (int j = 0; j < jumlahBarang; j++)
     {
         cout << "Nama Barang : " << Barang[j].nama << endl;
+        cout << "Id          : " << Barang[j].idBarang.huruf << Barang[j].idBarang.angka << endl;  
         cout << "Stok        : " << Barang[j].stok << endl;
-        cout << "Id          : " << Barang[j].idBarang.huruf << Barang[j].idBarang.angka << endl;      
+        cout << "Kategori    : " << Barang[j].kategori << endl;
+        cout << "Rak         : " << Barang[j].rak << endl;
         cout << "------------------------------------" << endl;
     }
 }
@@ -321,9 +464,13 @@ void cariBarang()
     {
         system("cls");
         cout << "Nama Barang : " << Barang[index].nama << endl;
-        cout << "Stok        : " << Barang[index].stok << endl;
         cout << "Id          : " << Barang[index].idBarang.huruf << Barang[index].idBarang.angka << endl;  
-    }else{
+        cout << "Stok        : " << Barang[index].stok << endl;
+        cout << "Kategori    : " << Barang[index].kategori << endl;
+        cout << "Rak         : " << Barang[index].rak << endl;  
+    }
+    else
+    {
         cout << "============================================\n";
         cout << "----------Barang tidak ditemukan!-----------\n";
     }
@@ -335,9 +482,45 @@ void menuPerbaruiBarang()
     cout << "====================================\n";
     cout << "| Pilih Data yang Ingin Diperbarui |\n";
     cout << "====================================\n";
-    cout << "| 1. Stok                          |\n";
-    cout << "====================================\n";   
-    cout << "Pilih menu : "; cin >> menuPerbarui;    
+    cout << "| 1. Nama barang                   |\n";
+    cout << "| 2. Stok                          |\n";
+    cout << "| 3. Kategori                      |\n";
+    cout << "| 4. Lokasi rak                    |\n";   
+    cout << "====================================\n";
+    cout << "Pilih menu : ";
+    cin >> menuPerbarui;
+}
+
+void perbaruiNama()
+{
+    system("cls");
+    string namaBarang;
+    int j, index;
+    bool ada = false;
+    cout << "Nama barang         : ";
+    cin >> namaBarang;
+    for (j = 0; j < jumlahBarang; j++)
+    {
+        if (namaBarang == Barang[j].nama)
+        {
+            ada = true;
+            index = j;
+        }
+    }
+    if (ada)
+    {
+        string namaBaru;
+        cout << "Masukkan nama baru : ";
+        cin >> namaBaru;
+        Barang[index].nama = namaBaru;
+        cout << "============================================\n";
+        cout << "---------Harga berhasil diperbarui!---------\n";
+    }
+    else
+    {
+        cout << "============================================\n";
+        cout << "----------Barang tidak ditemukan!-----------\n";
+    }
 }
 
 void perbaruiStok()
@@ -371,17 +554,57 @@ void perbaruiStok()
         cout << "----------Barang tidak ditemukan!-----------\n";
     }
 }
-
-void perbaruiBarang()
+void perbaruiRak()
 {
+    system("cls");
+    string namaBarang;
+    int j, index;
+    bool ada = false;
+    cout << "Nama barang        : ";
+    cin >> namaBarang;
+    for (j = 0; j < jumlahBarang; j++)
+    {
+        if (namaBarang == Barang[j].nama)
+        {
+            ada = true;
+            index = j;
+        }
+    }
+    if (ada)
+    {
+        int rakBaru;
+        cout << "Masukkan lokasi rak baru (1-25) : ";
+        cin >> rakBaru;
+        Barang[index].rak = rakBaru;
+        cout << "============================================\n";
+        cout << "---------Stok berhasil diperbarui!----------\n";
+    }
+    else
+    {
+        cout << "============================================\n";
+        cout << "----------Barang tidak ditemukan!-----------\n";
+    }
+}
+
+void perbaruiBarang(){
     menuPerbaruiBarang();
-    switch(menuPerbarui){
-        case 1  : perbaruiStok();  break;        
-        default : system("cls");
-            cout << "==================\n";
-            cout << "| Menu tidak ada |\n";
-            cout << "==================\n";
-        break;  
+    switch (menuPerbarui)
+    {
+    case 1:
+        perbaruiNama();
+        break;
+    case 2:
+        perbaruiStok();
+        break;
+    case 3:
+        perbaruiRak();
+        break;
+    default:
+        system("cls");
+        cout << "==================\n";
+        cout << "| Menu tidak ada |\n";
+        cout << "==================\n";
+        break;
     }
 }
 
